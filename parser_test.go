dda3f643f673dad9573a0aec3ca9d0df4c2686cf
Blake Wilson <bwilson025@gmail.com>
2015-05-31 23:34:46 -0500

    Add another parser test

--- parser_test.go
+++ parser_test.go
@@ -29,6 +29,11 @@ func TestVariables(t *testing.T) {
 	res, err = EvalExpression("x + y")
 	assert.Nil(t, err)
 	assert.Equal(t, float64(7), res.Eval(ctx))
+
+	ctx.AssignVariable("z", 5)
+	res, err = EvalExpression("x*y^z")
+	assert.Nil(t, err)
+	assert.Equal(t, float64(3072), res.Eval(ctx))
 }
 
 func TestTokenize(t *testing.T) {
